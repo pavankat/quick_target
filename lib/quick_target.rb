4cad6c10fc326844e1c3747070e469e585839c99
Pavan Katepalli <pavankat@gmail.com>
2016-01-01 04:26:20 -0500

    hello more complicated

--- lib/quick_target.rb
+++ lib/quick_target.rb
@@ -1,5 +1,21 @@
 class QuickTarget
-  def self.hi
-    puts "Hello world!"
+  def self.hi(language = "english")
+    translator = Translator.new(language)
+    translator.hi
+  end
+end
+
+class QuickTarget::Translator
+  def initialize(language)
+    @language = language
+  end
+
+  def hi
+    case @language
+    when "spanish"
+      "hola mundo"
+    else
+      "hello world"
+    end
   end
 end
